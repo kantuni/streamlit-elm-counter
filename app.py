08bba26d17857ec6467b542bb71fda57565c6810
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-12-03 11:38:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import streamlit.components.v1 as components
 
 counter = components.declare_component(
     "counter",
-    # Development:
-    # url="http://localhost:3000/",
     # Production:
     path="frontend/build/",
+    # Development:
+    # url="http://localhost:3000/",
 )
 
 count = counter(key="count", default=0)
